a0f31928003c0b6f17a6abae329d99565de61d47
Demin Ivan <DeminIV@suek.ru>
2026-05-04 22:01:41 +0700

    feat: docker + yaml

--- BunkerAPI/Program.cs
+++ BunkerAPI/Program.cs
@@ -2,10 +2,17 @@ using BunkerAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Render (и другие PaaS) задают порт через переменную PORT.
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrWhiteSpace(port))
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ICardService, CardService>();
 builder.Services.AddSingleton<IGameSessionService, GameSessionService>();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(o =>
+    o.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
 var app = builder.Build();
 
@@ -15,6 +22,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseAuthorization();
 app.MapControllers();
 
